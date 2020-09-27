bb683114a1ffdf7029ee854796792c4f1e4554d1
Bjørn Vidar Dahle <bvidar@dahle.me>
2020-09-27 11:54:27 +0200

    file styling, initial schema and resolver

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
-import 'dotenv/config.js';
-import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log(process.env.MY_SECRET);
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+const schema = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        username: String!
+    }
+`;
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: 'Bjørn Vidar Dahle',
+            };
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 4000 }, () => {
+    console.log('Apollo Server on http://localhost:4000/graphql');
+});
